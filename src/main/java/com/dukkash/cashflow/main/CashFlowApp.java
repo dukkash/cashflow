43b3f2e2e29e67c9d8420af10ca02d26807e1df0
Dukkash <dukkash@gmail.com>
2018-05-08 11:51:40 +0300

    Added spring boot

--- src/main/java/com/dukkash/cashflow/main/CashFlowApp.java
+++ src/main/java/com/dukkash/cashflow/main/CashFlowApp.java
@@ -1,8 +1,13 @@
 package com.dukkash.cashflow.main;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
 public class CashFlowApp {
 
     public static void main(String[] args) {
         System.out.println("Hello from CashFlow App!");
+        SpringApplication.run(CashFlowApp.class, args);
     }
 }
