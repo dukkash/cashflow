873ac3279dd3ec845af577a3177f4cee7ce9f44a
Dukkash <dukkash@gmail.com>
2018-05-08 19:37:59 +0300

    added test controller

--- src/main/java/com/dukkash/cashflow/main/CashFlowApp.java
+++ src/main/java/com/dukkash/cashflow/main/CashFlowApp.java
@@ -2,7 +2,12 @@ package com.dukkash.cashflow.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
+@ComponentScan
 @EnableAutoConfiguration
 public class CashFlowApp {
 
